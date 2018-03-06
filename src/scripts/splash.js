f2560dd78dfe39068b79bc07e5ae2e9f16c7f703
William Sedlacek <wsedlacek@high-sierra.lan>
2018-03-06 08:52:28 -0600

    Cleanup Animation and Architure

--- src/scripts/splash.js
+++ src/scripts/splash.js
@@ -2,5 +2,6 @@
 document.addEventListener('end-splash', e => endSplash());
 
 function endSplash() {
-  document.getElementById("splash").setAttribute('style', 'display: none');
+  //document.getElementById("splash").setAttribute('style', 'display: none');
+  document.getElementById("splash").setAttribute('class', 'loader hidden');
 }
