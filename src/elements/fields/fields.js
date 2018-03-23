0e8efd7cbe4e0432e726dfbf6009a2695dddb1be
William Sedlacek <wsedlacek@null.lan>
2018-03-23 13:39:49 -0500

    Cleanup Tabs and Add Josn Based Fields

---
+++ src/elements/fields/fields.js
@@ -0,0 +1,19 @@
+var json = {
+  "entries": [{
+    "type": "field",
+    "key": "noc",
+    "label": "Name of Caller",
+    "tab": true,
+    "rows": 1,
+    "pre": "-NOC [",
+    "post": "]"
+  }, {
+    "type": "field",
+    "key": "noa",
+    "label": "Name on the Account",
+    "tab": false,
+    "rows": 1,
+    "pre": "-NOA [",
+    "post": "]"
+  }]
+}
