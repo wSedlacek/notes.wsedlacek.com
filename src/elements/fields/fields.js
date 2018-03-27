afeb5148e8f2aa78f8cd40ebc4d4d99ae87e3cbb
William Sedlacek <wsedlacek@null.lan>
2018-03-26 22:29:25 -0500

    Deleting Tabs

--- src/elements/fields/fields.js
+++ src/elements/fields/fields.js
@@ -2,14 +2,14 @@ var json = {
   "entries": [{
     "type": "div",
     "key": "info",
-    "text": "====INFO===="
+    "text": "===========Info=========="
   }, {
     "type": "field",
     "key": "noc",
     "label": "Name of Caller",
     "tab": true,
     "rows": 1,
-    "pre": "-NOC: [",
+    "pre": "-NOC:    [",
     "post": "]",
     "linked": "noa"
   }, {
@@ -18,8 +18,169 @@ var json = {
     "label": "Name on the Account",
     "tab": false,
     "rows": 1,
-    "pre": "-NOA: [",
+    "pre": "-NOA:    [",
     "post": "]",
     "linked": "noc"
-  }]
+  }, {
+    "type": "field",
+    "key": "btn",
+    "label": "Billing Telephone Number",
+    "tab": false,
+    "rows": 1,
+    "pre": "-BTN:    [",
+    "post": "]",
+    "linked": "cbn"
+  }, {
+    "type": "field",
+    "key": "cbn",
+    "label": "Callback Number",
+    "tab": false,
+    "rows": 1,
+    "pre": "-CBN:   [",
+    "post": "]",
+    "linked": "btn"
+  }, {
+    "type": "field",
+    "key": "ban",
+    "label": "Billing Account Number",
+    "tab": false,
+    "rows": 1,
+    "pre": "-BAN: [",
+    "post": "]",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "rx",
+    "label": "RX Session Number",
+    "tab": false,
+    "rows": 1,
+    "pre": "-RX:     [",
+    "post": "]",
+    "linked": ""
+  }, {
+    "type": "div",
+    "key": "billing",
+    "text": "-------------------------"
+  }, {
+    "type": "field",
+    "key": "verifation",
+    "label": "Verifation Method",
+    "tab": false,
+    "rows": 1,
+    "pre": "-Ver:    [",
+    "post": "]",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "msp",
+    "label": "Service Level",
+    "tab": false,
+    "rows": 1,
+    "pre": "-MSP:    [",
+    "post": "]",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "agency",
+    "label": "Agency",
+    "tab": false,
+    "rows": 1,
+    "pre": "-Agency:  [",
+    "post": "]",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "order",
+    "label": "Order Number",
+    "tab": false,
+    "rows": 1,
+    "pre": "-Ord:    [",
+    "post": "]",
+    "linked": ""
+  }, {
+    "type": "div",
+    "key": "pts",
+    "text": "-------------------------"
+  }, {
+    "type": "field",
+    "key": "link",
+    "label": "Account Link",
+    "tab": false,
+    "rows": 1,
+    "pre": "-Link:   [",
+    "post": "]",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "vtr",
+    "label": "VTR PIN",
+    "tab": false,
+    "rows": 1,
+    "pre": "-VTR:   [",
+    "post": "]",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "lmir",
+    "label": "LMIR PIN",
+    "tab": false,
+    "rows": 1,
+    "pre": "-LMIR:   [",
+    "post": "]",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "pcinfo",
+    "label": "PC Info",
+    "tab": false,
+    "rows": 2,
+    "pre": "---------PC-INFO---------\n",
+    "post": "",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "problem",
+    "label": "Problem ~ What the customer says the problem is verbatium",
+    "tab": false,
+    "rows": 3,
+    "pre": "==========Problem==========\n",
+    "post": "",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "issue",
+    "label": "Simplified Issue ~ A very simple and accurate description of the actual problem",
+    "tab": false,
+    "rows": 3,
+    "pre": "~~~~~~~~~~~~~~~~~~~~~~~~~\n",
+    "post": "",
+    "linked": "\n"
+  }, {
+    "type": "field",
+    "key": "steps",
+    "label": "Steps Taken to Resolve the Issue",
+    "tab": false,
+    "rows": 5,
+    "pre": "==========Steps==========\n",
+    "post": "",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "todo",
+    "label": "Todo",
+    "tab": false,
+    "rows": 3,
+    "pre": "\n<<<<<<<<<\nTodo:\n",
+    "post": "\n",
+    "linked": ""
+  }, {
+    "type": "field",
+    "key": "resolution",
+    "label": "Resolution",
+    "tab": false,
+    "rows": 2,
+    "pre": "=======Resolution========\n",
+    "post": "",
+    "linked": ""
+  }, ]
 }
